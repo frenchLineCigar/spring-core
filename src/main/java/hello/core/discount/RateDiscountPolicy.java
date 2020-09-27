4348bc4af8b4e6ddac6a943ec77624d3cc09e201
Lucy <frenchline707@gmail.com>
2020-09-28 06:44:37 +0900

    새로운 할인 정책 개발 (정률 할인)
    --------------------
    3. 스프링 핵심 원리 이해 2 - 객체 지향 원리 적용 - 새로운 할인 정책 개발
    
    새로운 할인 정책 확장
    : 주문 금액당 할인하는 정률 % 할인으로 변경하고자 함

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,27 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+/**
+ * Created by frenchline707@gmail.com on 2020-09-28
+ * Blog : http://frenchline707.tistory.com
+ * Github : http://github.com/frenchLineCigar
+ */
+
+/**
+ * 할인 정책 : 정률 할인
+ */
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}

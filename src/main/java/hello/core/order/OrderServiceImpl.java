d25c947148988d7ed5459a99bdab1563bd3273fb
Lucy <frenchline707@gmail.com>
2020-09-28 07:08:48 +0900

    새로운 할인 정책 적용과 문제점
    인터페이스에만 의존하도록 설계와 코드 변경
    그런데 구현체가 없어 실행 시 NPE(NullPointerException)발생
    -> 클라이언트인 OrderServiceImpl에 누군가가 DiscountPolicy의 구현 객체를 대신 생성하고 주입해줘야 한다
    
    --------------------
    3. 스프링 핵심 원리 이해 2 - 객체 지향 원리 적용 - 새로운 할인 정책 적용과 문제점
    
    OCP, DIP 위반 -> 추상에만 의존하도록 변경(인터페이스에만 의존)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -15,18 +14,21 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy; //인터페이스에만 의존하도록 코드 변경
 
     /**
-     * 
      * 주문 생성
-     * @return
      */
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId); //회원 정보 조회
-        int discountPrice = discountPolicy.discount(member, itemPrice); //할인 정책 조회
+        int discountPrice = discountPolicy.discount(member, itemPrice); //할인 정책 조회 -> 실행 시 NullPointerException 발생
+
+        // 구현체가 없는데 어떻게 코드를 실행할 수 있을까?
+        // : 누군가가 클라이언트인 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해주어야 한다
 
         return new Order(memberId, itemName, itemPrice, discountPrice); //생성된 주문을 반환
     }
+
+
 }

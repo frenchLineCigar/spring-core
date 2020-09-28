064c6ff64da51f46f1e631b55365bdb9d430fa10
Lucy <frenchline707@gmail.com>
2020-09-28 21:09:13 +0900

    스프링 빈 조회 - 기본
    * ApplicationContextBasicFindTest : 스프링 컨테이너에서 스프링 빈을 찾는 가장 기본적인 조회 방법
    
    -----------------------
    4. 스프링 컨테이너와 스프링 빈 - 스프링 빈 조회 - 기본
    
    [정리]
    스프링 컨테이너에서 스프링 빈을 찾는 가장 기본적인 조회 방법
    1) ac.getBean(빈이름, 타입)
    2) ac.getBean(타입)
    3) 조회 대상 스프링 빈이 없으면 예외 발생 : NoSuchBeanDefinitionException: No bean named 'xxxxx' available.

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,54 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Created by frenchline707@gmail.com on 2020-09-28
+ * Blog : http://frenchline707.tistory.com
+ * Github : http://github.com/frenchLineCigar
+ */
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+//        System.out.println("memberService = " + memberService);
+//        System.out.println("memberService.getClass() = " + memberService.getClass());
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class); //구체 타입 의존이므로 유연성이 떨어진다(상황에 따라 사용)
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+//        ac.getBean("xxxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class)); // assertThrows(터져야하는 예외타입, 어떤로직을 실행했을때)
+    }
+}

ee405892ec63a3d6858579616af84a3974616868
Lucy <frenchline707@gmail.com>
2020-09-29 15:20:19 +0900

    5. 싱글톤 컨테이너 - 싱글톤 패턴
    SingletonService
    SingletonTest
    
     * 앞에서 만들었던 순수한 DI 컨테이너가 가진 문제점을 살펴보고
     * 싱글톤 패턴으로 바꿔보고 여기서 나오는 문제점 또한 살펴본다
     * 나중에 스프링과 비교해본다.
    
    싱글톤 패턴을 적용한 객체 사용 : 호출할 때 마다 같은 객체 인스턴스를 반환하는 것을 확인
    (ref. 싱글톤 패턴을 구현하는 방법은 여러가지. 예제는 객체를 미리 생성해두는 가장 단순하고 안전한 방법 선택)
    
    싱글톤 패턴을 적용하면 고객의 요청이 올 때 마다 객체를 새로 생성하는 것이 아니라,
    이미 만들어진 객체를 공유해서 효율적으로 사용할 수 있다.
    하지만 싱글톤 패턴은 다음과 같은 수 많은 문제점들을 가지고 있다.
    
    싱글톤 패턴 문제점
    * 싱글톤 패턴을 구현하는 코드 자체가 많이 들어간다
    * 의존관계상 클라이언트가 구체 클래스에 의존한다 -> DIP 위반 (ex. 구체클래스.getInstance() 로 객체를 꺼냄)
    * 클라이언트가 구체 클래스에 의존해서 OCP 원칙을 위반할 가능성이 높다
    * 유연하게 테스트하기 어렵다
    * 내부 속성을 변경하거나 초기화 하기 어렵다
    * private 생성자로 자식 클래스를 만들기 어렵다
    * 결론적으로 유연성이 떨어진다 (DI 등을 적용하기 어려워짐. ex.구체클래스.getInstance() 다 해줘야 됨)
    * 안티패턴으로 불리기도 한다
    
    스프링 컨테이너를 쓰면 기본적으로 객체를 싱글톤으로 만들어서 관리해준다
    : 객체의 재활용, 성능관리
    
    싱글톤 패턴의 장점은 공유(share)를 할 수 있고, 확실하게 객체 하나가 있다는 게 보장이 되지만
    위처럼 수많은 단점을 갖고 있다.
    그런데 스프링 프레임워크는 싱글톤 패턴의 문제점을 전부 다 해결하고, 객체를 싱글톤으로 관리한다
    싱글톤이 가진 단점을 다 제거하면서 객체를 싱글톤으로 관리해준다

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,9 +2,10 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by frenchline707@gmail.com on 2020-09-29
@@ -38,7 +39,30 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2); //MemberServiceImpl@5c6648b0
 
         //memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        //private으로 생성자를 막아두었다. 컴파일 오류가 발생한다.
+        //new SingletonService();
+
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        //참조값이 같은 것을 확인 : 같은 객체의 인스턴스가 반환
+        System.out.println("singletonService1 = " + singletonService1); //SingletonService@2a65fe7c
+        System.out.println("singletonService2 = " + singletonService2); //SingletonService@2a65fe7c
+
+        // singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //same : == (객체 인스턴스, 참조값 비교)
+        //equal : equals method
+
+        singletonService1.logic();
     }
 
 }

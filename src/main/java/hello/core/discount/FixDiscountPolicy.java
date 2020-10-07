1b17a978b06e3ab07ad08a4873a1376fc5bd5022
Lucy <frenchline707@gmail.com>
2020-10-07 21:36:08 +0900

    7. 의존관계 자동 주입 - 조회한 빈이 모두 필요할 때, List, Map
    혼자 이거저거 복습 및 연습

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -13,7 +13,9 @@ import org.springframework.stereotype.Component;
 /**
  * 할인 정책 : 고정 할인 정책
  */
-@Component("fix")
+
+//@Component("fix")
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; //1000원 할인
--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -30,11 +30,13 @@ public class AllBeanTest {
         Member member = new Member(1L, "userA", Grade.VIP); //VIP
         assertThat(discountService).isInstanceOf(DiscountService.class);
 
-        int fixDiscountPrice = discountService.discount(member, 20000, "fix");
+        int fixDiscountPrice = discountService.discountFindByPrefix(member, 20000, "fix");
+//        int fixDiscountPrice = discountService.discount(member, 20000, "fix");
 //        int fixDiscountPrice = discountService.discount(member, 20000, "fixDiscountPolicy");
         assertThat(fixDiscountPrice).isEqualTo(1000); //고정 할인 정책에서는 VIP의 경우 1000원이 할인돼야 함
 
-        int rateDiscountPrice = discountService.discount(member, 50000, "rate");
+        int rateDiscountPrice = discountService.discountFindByPrefixWithJava8(member, 50000, "rate");
+//        int rateDiscountPrice = discountService.discount(member, 50000, "rate");
 //        int rateDiscountPrice = discountService.discount(member, 50000, "rateDiscountPolicy");
         assertThat(rateDiscountPrice).isEqualTo(5000); //정률 할인 정책에서는 VIP의 경우 판매금액의 10%가 할인돼야 함
 
@@ -43,6 +45,14 @@ public class AllBeanTest {
     static class DiscountService { // allBeanTest.DiscountService
         private final Map<String, DiscountPolicy> policyMap;
         private final List<DiscountPolicy> policies;
+
+//        @Autowired //자동 주입 시 커스텀 애노테이션 옵션 활용
+//        public DiscountService(@MainDiscountPolicy Map<String, DiscountPolicy> policyMap, @MainDiscountPolicy List<DiscountPolicy> policies) {
+//            this.policyMap = policyMap;
+//            this.policies = policies;
+//            System.out.println("policyMap = " + policyMap); //only RateDiscountPolicy <- caused by @MainDiscountPolicy
+//            System.out.println("policies = " + policies);
+//        }
 
         @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
@@ -55,6 +65,33 @@ public class AllBeanTest {
         public int discount(Member member, int price, String discountCode) { //문자열 모두 매칭 (강의 코드) <- rateDiscountPolicy, fixDiscountPolicy
             DiscountPolicy discountPolicy = policyMap.get(discountCode);
             return discountPolicy.discount(member, price);
+        }
+
+        /* 걍 해본 것 - 시작 문자열(prefix) : rate, fix 로 매칭 조회 */
+        public int discountFindByPrefix(Member member, int price, String discountCode) {
+            final int NOT_FOUND_POLICY = 0; //이렇게 값을 지정하면 안될 것 같은데...어떻게 해야하지..
+            for (String policy : policyMap.keySet()) {
+                if (policy.startsWith(discountCode)) {
+                    DiscountPolicy discountPolicy = policyMap.get(policy);
+                    return discountPolicy.discount(member, price);
+                }
+            }
+            return NOT_FOUND_POLICY;
+        }
+
+        /* 걍 해본 것 - 자바 8 사용, 시작 문자열(prefix) : rate, fix 로 매칭 조회 */
+        public int discountFindByPrefixWithJava8(Member member, int price, String discountCode) {
+//            final int NOT_FOUND_POLICY = 0;
+            return policyMap.keySet()
+                    .stream()
+                    .filter(policy -> policy.startsWith(discountCode))
+                    .map(policyMap::get)
+                    .findFirst()
+                    .map(discountPolicy -> discountPolicy.discount(member, price))
+                    .orElseThrow();
+//                    .orElseGet(() -> NOT_FOUND_POLICY);
+//                    .orElse(NOT_FOUND_POLICY);
+
         }
 
     }

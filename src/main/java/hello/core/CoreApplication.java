2b291477e561b8bd318e00edb5b04de42ebd69c4
Lucy <frenchline707@gmail.com>
2020-10-10 16:30:19 +0900

    원상복구

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -1,67 +1,17 @@
 package hello.core;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.stereotype.Component;
 
-import static org.springframework.context.annotation.ComponentScan.Filter;
-
-@SpringBootApplication //AnnotationConfigServletWebServerApplicationContext 기반 구동 (spring-boot-start-web 추가 시)
-@ComponentScan(
-		excludeFilters = {
-				@Filter(type = FilterType.ANNOTATION, classes = Configuration.class), //충돌 피하려고 다른 설정 제외
-		}
-)
+@SpringBootApplication
 public class CoreApplication {
 
-	/**
-	 * 주입 받아서 직접 확인
-	 */
-	@Autowired
-	public CoreApplication(ApplicationContext applicationContext) {
-		System.out.println("applicationContext = " + applicationContext);
-		System.out.println("applicationContext.getClass() = " + applicationContext.getClass()); //AnnotationConfigServletWebServerApplicationContext
-	}
-
-	/**
-	 * 스프링 부트에 웹 라이브러리가 추가되면, 웹과 관련된 추가 설정과 환경들이 필요하므로
-	 * `AnnotationConfigServletWebServerApplicationContext` 를 기반으로 애플리케이션 구동
-	 */
 	public static void main(String[] args) {
 		System.out.println("==== CoreApplication.main ====");
 		ApplicationContext ac = SpringApplication.run(CoreApplication.class, args);
 		System.out.println("[main] ac = " + ac);
-		System.out.println("[main] ac.getClass() = " + ac.getClass()); //AnnotationConfigServletWebServerApplicationContext
+		System.out.println("[main] ac.getClass() = " + ac.getClass()); //AnnotationConfigApplicationContext
 	}
 
-	/**
-	 * 1. `spring-boot-starter-web` 라이브러리를 추가하면 스프링 부트는 내장 톰캣 서버(Embedded Tomcat Server)를 활용해서
-	 * 	웹 서버와 스프링을 함께 실행시킨다
-	 * 2. 스프링 부트는 웹 라이브러리가 없으면, 우리가 지금까지 학습한 `AnnotationConfigApplicationContext` 를 기반으로 애플리케이션을 구동한다. 웹 라이브러리가 추가되면, 웹과 관련된 추가 설정과 환경들이 필요하므로
-	 * 	`AnnotationConfigServletWebServerApplicationContext` 를 기반으로 애플리케이션을 구동한다.
-	 * * 참고 : 스프링 부트 테스트( @SpringBootTest )는 `GenericWebApplicationContext` 를 기반으로 구동
-	 */
-
-	@Component
-	static class AppContextView {
-
-		@Autowired
-		public AppContextView(ApplicationContext ac) {
-			System.out.println("==== AppContextView.AppContextView ====");
-			System.out.println("ac = " + ac + "\n, object = " + ac.getClass()); //AnnotationConfigServletWebServerApplicationContext
-		}
-
-		@Autowired
-		public void showContext(ApplicationContext ac) {
-			System.out.println("==== AppContextView.showContext ====");
-			System.out.println("[showContext] ac = " + ac);
-			System.out.println("[showContext] ac.getClass() = " + ac.getClass()); //AnnotationConfigServletWebServerApplicationContext
-		}
-
-	}
 }
